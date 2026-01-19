44b877d91a5603010a93d563169c86e0453c06f1
Aulia <auliarachman81@guru.smk.belajar.id>
2026-01-19 14:02:28 +0700

    Judul Admin Di Ubah

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -25,7 +25,7 @@ $result_pengaduan = mysqli_query($conn, $query_pengaduan);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard Admin - Pengaduan Sekolah</title>
+    <title>Dashboard Admin - Pengaduan Fasilitas Sekolah</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
     <style>
